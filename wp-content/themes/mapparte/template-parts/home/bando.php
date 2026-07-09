9ac0cd9cb150cec5ce928558b6e670daac8bde3c
“e.gennuso” <emanuele.gennuso@rai.it>
2026-07-09 16:41:46 +0200

    Hide FVG funding block

--- wp-content/themes/mapparte/template-parts/home/bando.php
+++ wp-content/themes/mapparte/template-parts/home/bando.php
@@ -1,25 +1,4 @@
-<div class="home-bando" style="padding:40px">
-
-    <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-6">
-                    <img class="bg-divider" style="margin:0 auto" src="<?php echo get_template_directory_uri();?>/assets/images/loghi_fvg.png" alt="divider" />      
-                </div>
-                <div class="media-content col-12 col-md-6">
-                    <div class="mbr-section-text">
-                        <h6 class="section-subttl"><?php echo __("Il progetto MAPPARTE PRO ha ottenuto un finanziamento di 71.999,75 € dalla Regione Autonoma Friuli Venezia Giulia sul bando POR FESR 2014-2020, Linea di intervento 2.1.b.1 bis «Concessione di sovvenzioni per il finanziamento di programmi personalizzati di pre-incubazione e incubazione d’impresa, finalizzati alla realizzazione di progetti di creazione o di sviluppo di nuove imprese caratterizzate da una significativa valenza o da un rilevante connotato culturale e/o creativo»","mapparte"); ?><br></h6>
-                    </div>
-                </div>
-                      
-            </div>
-        </div>
-        <div class="mt-5 col-sm-3 col-12 col-lg-2 space-btn">
-            <a class="read-more" href="<?php echo get_template_directory_uri();?>/assets/bando_mapparte.pdf" target="_blank"><img src="/wp-content/themes/mapparte/assets/images/more.svg" alt="btn">
-                <p><?php echo __("Scarica il Poster","mapparte"); ?></p>
-            </a>
-        </div>
-
-    
-
-
-</div> 
+<?php
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
